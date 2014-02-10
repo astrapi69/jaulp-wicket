f0da4bc0983eae4e4bc1bb314fd69fe620d46a40
astrapi69 <asterios.raptis@gmx.net>
2014-02-10 13:55:58 +0100

    HTML5 shim, for IE6-8 support of HTML5 elements

--- jaulp.wicket.base/src/main/java/org/jaulp/wicket/base/GenericBasePage.java
+++ jaulp.wicket.base/src/main/java/org/jaulp/wicket/base/GenericBasePage.java
@@ -204,6 +204,7 @@
  */
 package org.jaulp.wicket.base;
 
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.StringResourceModel;
@@ -222,8 +223,14 @@ public abstract class GenericBasePage<T> extends AbstractGenericBasePage<T> {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	/** The title. */
-	private final IModel<String> title;
+	/** The meta tag content for description. */
+	private IModel<String> description;
+
+	/** The meta tag content for keywords. */
+	private IModel<String> keywords;
+
+	/** The meta tag content for the title. */
+	private IModel<String> title;
 
 	/**
 	 * Instantiates a new base page.
@@ -240,26 +247,76 @@ public abstract class GenericBasePage<T> extends AbstractGenericBasePage<T> {
 	 */
 	public GenericBasePage(final PageParameters parameters) {
 		super(parameters);
-		add(new Label("title", title = newTitle()));
+	}
+
+    /**
+     * {@inheritDoc}
+     */
+	@Override
+	protected void onBeforeRender() {
+	    // set content of the <title> tag
+		addOrReplace(new Label("title", title = newTitle()));
+	    // set content attribute of the <meta name="keywords"> tag
+		addOrReplace(new Label("keywords", "").add(new AttributeAppender("content", keywords = newKeywords(), " ")));
+	    // set content attribute of the <meta name="description"> tag
+		addOrReplace(new Label("description", "").add(new AttributeAppender("content", description = newDescription(), " ")));
+	    super.onBeforeRender();
 	}
 
 	/**
-	 * Factory method that can be overwritten for new page title.
-	 * 
-	 * @return the i model
+	 * Gets the meta tag content for the description.
+	 *
+	 * @return the description
 	 */
-	protected IModel<String> newTitle() {
-		return new StringResourceModel("page.title", this, null, "Home page");
-
+	public IModel<String> getDescription() {
+		return description;
 	}
 
 	/**
-	 * Gets the page title.
+	 * Gets the meta tag content for the keywords.
+	 *
+	 * @return the keywords
+	 */
+	public IModel<String> getKeywords() {
+		return keywords;
+	}
+
+	/**
+	 * Gets the meta tag content for the page title.
 	 * 
 	 * @return the page title
 	 */
 	public IModel<String> getTitle() {
 		return title;
+	}
+
+	/**
+	 * Factory method that can be overwritten for new meta tag content for
+	 * description.
+	 * 
+	 * @return the new <code>IModel<code>
+	 */
+	protected IModel<String> newDescription() {
+		return new StringResourceModel("page.meta.description", this, null, "");
+	}
+
+	/**
+	 * Factory method that can be overwritten for new meta tag content for
+	 * keywords.
+	 * 
+	 * @return the new <code>IModel<code>
+	 */
+	protected IModel<String> newKeywords() {
+		return new StringResourceModel("page.meta.keywords", this, null, "");
+	}
+
+	/**
+	 * Factory method that can be overwritten for new meta tag content for the title.
+	 * 
+	 * @return the new <code>IModel<code>
+	 */
+	protected IModel<String> newTitle() {
+		return new StringResourceModel("page.title", this, null, "Home page");
 	}
 
 }
