f6cadf55cba5ee22645ab39318b63dd4de4ea6ec
astrapi69 <asterios.raptis@gmx.net>
2014-02-12 11:55:28 +0100

    Added new constructors with page and page class arguments.

--- jaulp.wicket.behaviors/src/main/java/org/jaulp/wicket/behaviors/LinkBehavior.java
+++ jaulp.wicket.behaviors/src/main/java/org/jaulp/wicket/behaviors/LinkBehavior.java
@@ -207,7 +207,13 @@ package org.jaulp.wicket.behaviors;
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.util.lang.Args;
+import org.jaulp.wicket.base.utils.WicketComponentUtils;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class LinkBehavior adds the ability to a component to act as a link, </br> i.e.
  * like a tablerow tag( &lt;tr&gt; ) or a list item (&lt;li&gt;) the ability to act as a link. </br>For instance
@@ -242,16 +248,60 @@ public class LinkBehavior extends Behavior {
 	public LinkBehavior(final String onmouseoverColor,
 			final String onmouseoutColor, final String absolutePath) {
 		super();
+		Args.notNull(onmouseoverColor, "onmouseoverColor");
+		Args.notNull(onmouseoutColor, "onmouseoutColor");
+		Args.notNull(absolutePath, "absolutePath");
 		this.onmouseoverColor = onmouseoverColor;
 		this.onmouseoutColor = onmouseoutColor;
 		this.absolutePath = absolutePath;
 	}
+	
+	/**
+	 * Instantiates a new tablerow link behavior.
+	 *
+	 * @param onmouseoverColor the onmouseover color
+	 * @param onmouseoutColor the onmouseout color
+	 * @param targetPage the target page
+	 * @param pageParameters the page parameters
+	 */
+	public LinkBehavior(final String onmouseoverColor,
+			final String onmouseoutColor, 
+			final Class<? extends WebPage> targetPage, 
+			final PageParameters pageParameters) {
+		super();
+		Args.notNull(onmouseoverColor, "onmouseoverColor");
+		Args.notNull(onmouseoutColor, "onmouseoutColor");
+		Args.notNull(targetPage, "targetPage");
+		this.onmouseoverColor = onmouseoverColor;
+		this.onmouseoutColor = onmouseoutColor;
+		String url = RequestCycle.get().urlFor(targetPage, pageParameters).toString();
+		this.absolutePath = WicketComponentUtils.toAbsolutePath(url);
+	}
+	
+	/**
+	 * Instantiates a new tablerow link behavior.
+	 *
+	 * @param onmouseoverColor the onmouseover color
+	 * @param onmouseoutColor the onmouseout color
+	 * @param targetPage the target page
+	 * @param pageParameters the page parameters
+	 */
+	public LinkBehavior(final String onmouseoverColor,
+			final String onmouseoutColor, 
+			final WebPage targetPage, 
+			final PageParameters pageParameters) {
+		super();
+		Args.notNull(onmouseoverColor, "onmouseoverColor");
+		Args.notNull(onmouseoutColor, "onmouseoutColor");
+		Args.notNull(targetPage, "targetPage");
+		this.onmouseoverColor = onmouseoverColor;
+		this.onmouseoutColor = onmouseoutColor;
+		String url = RequestCycle.get().urlFor(targetPage.getClass(), pageParameters).toString();
+		this.absolutePath = WicketComponentUtils.toAbsolutePath(url);
+	}
 
 	/**
 	 * (non-Javadoc)
-	 * 
-	 * @see org.apache.wicket.behavior.AbstractBehavior#onComponentTag(org.apache.wicket.Component,
-	 *      org.apache.wicket.markup.ComponentTag)
 	 */
 	@Override
 	public void onComponentTag(final Component component, final ComponentTag tag) {
