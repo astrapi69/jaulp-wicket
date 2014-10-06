582592d086e2fac40325c002d19a3c5d83555680
astrapi69 <asterios.raptis@gmx.net>
2014-10-06 17:04:32 +0200

    New behavior for add google analytic js script.

---
+++ jaulp.wicket.behaviors/src/main/java/org/jaulp/wicket/behaviors/GoogleAnalyticsBehavior.java
@@ -0,0 +1,24 @@
+package org.jaulp.wicket.behaviors;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.jaulp.wicket.behaviors.AddJsResourceReferenceBehavior;
+
+/**
+ * The Class GoogleAnalyticsBehavior adds the application specific js script.
+ */
+public class GoogleAnalyticsBehavior extends AddJsResourceReferenceBehavior {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Instantiates a new google analytics behavior.
+	 *
+	 * @param pageClass
+	 *            the page class
+	 */
+	public GoogleAnalyticsBehavior(final Class<? extends WebPage> pageClass) {
+		super(pageClass, "gaq.js", "gaq");
+	}
+
+}
