40e229605ed9b9a39ce455ff249fffa972825b52
astrapi69 <asterios.raptis@gmx.net>
2014-10-10 12:59:44 +0200

    new ajax indicator loading panel.

---
+++ jaulp.wicket.components/src/main/java/de/alpharogroup/wicket/components/indicator/AjaxIndicatorLoadingPanel.java
@@ -0,0 +1,29 @@
+package de.alpharogroup.wicket.components.indicator;
+
+import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
+import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * The Class AjaxIndicatorLoadingPanel.
+ */
+public class AjaxIndicatorLoadingPanel extends Panel {
+
+
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+/**
+   * Instantiates a new loading indicator panel.
+   *
+   * @param id the id
+   */
+  public AjaxIndicatorLoadingPanel(String id) {
+    super(id);
+    setOutputMarkupId(true);
+    Image ajaxindicator = new Image("ajaxindicator", AbstractDefaultAjaxBehavior.INDICATOR);
+    add(ajaxindicator);
+  }
+}
