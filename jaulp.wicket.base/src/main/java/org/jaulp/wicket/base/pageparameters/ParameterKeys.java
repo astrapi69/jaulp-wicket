f94a4337ab30b565730de80103fbca0c0e95df49
astrapi69 <asterios.raptis@gmx.net>
2014-10-10 13:30:39 +0200

    constant class for general parameter keys.

---
+++ jaulp.wicket.base/src/main/java/org/jaulp/wicket/base/pageparameters/ParameterKeys.java
@@ -0,0 +1,21 @@
+package org.jaulp.wicket.base.pageparameters;
+/**
+ * The Class ParameterKeys contains constants for the <code>PageParameters</code> keys.
+ */
+public class ParameterKeys {
+
+	/** The Constant CONFIRMATION_CODE. */
+	public static final String CONFIRMATION_CODE = "forgotten";
+	
+	/** The Constant USER_ID. */
+	public static final String USER_ID = "USER_ID";
+
+	/** The Constant USERNAME. */
+	public static final String USERNAME = "username";
+
+	/** The Constant ACTION. */
+	public static final String ACTION = "action";
+
+	/** The Constant INFO_MESSAGE. */
+	public static final String INFO_MESSAGE = "info_massage";
+}
