db76023698c26017fe313ad0e157924900a7cd23
astrapi69 <asterios.raptis@gmx.net>
2014-10-10 13:31:04 +0200

    new panel for reset password

---
+++ jaulp.wicket.components/src/main/java/de/alpharogroup/wicket/components/sign/in/password/reset/AbstractResetPasswordPanel.java
@@ -0,0 +1,37 @@
+package de.alpharogroup.wicket.components.sign.in.password.reset;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.jaulp.wicket.base.pageparameters.ParameterKeys;
+
+
+/**
+ * The class ResetPasswordPanel.
+ * 
+ * @author Asterios Raptis
+ */
+public abstract class AbstractResetPasswordPanel extends Panel {
+
+	/**
+	 * The serialVersionUID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AbstractResetPasswordPanel(final String id, final PageParameters parameters) {
+		super(id);
+		ResetPasswordModel model = ResetPasswordModel
+				.builder()
+				.username(parameters.get(ParameterKeys.USERNAME).toString())
+				.confirmationCode(parameters.get(ParameterKeys.CONFIRMATION_CODE).toString().trim())
+				.build();
+		onReset(model.getUsername(), model.getConfirmationCode());
+	}
+
+	public AbstractResetPasswordPanel(final String id, final IModel<ResetPasswordModel> model) {
+		super(id, model);
+		onReset(model.getObject().getUsername(), model.getObject().getConfirmationCode());
+	}
+
+	protected abstract void onReset(String username, String confirmationCode);
+}
---
+++ jaulp.wicket.components/src/main/java/de/alpharogroup/wicket/components/sign/in/password/reset/ResetPasswordModel.java
@@ -0,0 +1,24 @@
+package de.alpharogroup.wicket.components.sign.in.password.reset;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Builder;
+
+@Getter 
+@Setter 
+@EqualsAndHashCode 
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResetPasswordModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String username;
+	private String confirmationCode;
+}
