83f79fe243408bbd04e6dbb7041f839b6ee14329
astrapi69 <asterios.raptis@gmx.net>
2014-10-14 14:59:55 +0200

    Added junit dependency for the WicketTester.

--- jaulp.wicket.components.examples/src/test/java/de/alpharogroup/wicket/components/examples/TestHomePage.java
+++ jaulp.wicket.components.examples/src/test/java/de/alpharogroup/wicket/components/examples/TestHomePage.java
@@ -15,19 +15,19 @@ public class TestHomePage
 {
 	private WicketTester tester;
 
-//	@BeforeMethod
-//	public void setUp()
-//	{
-//		tester = new WicketTester(new WicketApplication());
-//	}
-//
-//	@Test
-//	public void homepageRendersSuccessfully()
-//	{
-//		//start and render the test page
-//		tester.startPage(HomePage.class);
-//
-//		//assert rendered page class
-//		tester.assertRenderedPage(HomePage.class);
-//	}
+	@BeforeMethod
+	public void setUp()
+	{
+		tester = new WicketTester(new WicketApplication());
+	}
+
+	@Test
+	public void homepageRendersSuccessfully()
+	{
+		//start and render the test page
+		tester.startPage(HomePage.class);
+
+		//assert rendered page class
+		tester.assertRenderedPage(HomePage.class);
+	}
 }
