93156af57af1da2aa1fc81043c6a5c7f0920caf4
astrapi69 <asterios.raptis@gmx.net>
2014-10-15 18:39:52 +0200

    Created an implementation of ChainableStatement that is serializable and can be build with a Builder class.

---
+++ jaulp.wicket.behaviors/src/main/java/org/jaulp/wicket/behaviors/BuildableChainableStatement.java
@@ -0,0 +1,58 @@
+package org.jaulp.wicket.behaviors;
+
+import java.io.Serializable;
+
+import org.odlabs.wiquery.core.javascript.ChainableStatement;
+
+public class BuildableChainableStatement implements ChainableStatement,
+		Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The statement label.
+	 */
+	private String label;
+
+	/**
+	 * The statement args.
+	 */
+	private CharSequence[] args;
+
+	@Override
+	public String chainLabel() {
+		return label;
+	}
+
+	@Override
+	public CharSequence[] statementArgs() {
+		return args;
+	}
+
+	public static class Builder {
+		private String label;
+		private CharSequence[] args;
+
+		public Builder label(String label) {
+			this.label = label;
+			return this;
+		}
+
+		public Builder args(CharSequence... args) {
+			this.args = args;
+			return this;
+		}
+
+		public BuildableChainableStatement build() {
+			return new BuildableChainableStatement(this);
+		}
+	}
+
+	/**
+	 * Creates a new instance of {@link BuildableChainableStatement}.
+	 */
+	private BuildableChainableStatement(Builder builder) {
+		this.label = builder.label;
+		this.args = builder.args;
+	}
+}
