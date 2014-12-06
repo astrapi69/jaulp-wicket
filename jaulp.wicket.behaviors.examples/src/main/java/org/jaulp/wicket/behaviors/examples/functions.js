7cf0a520c0431363fe247cd4b283160520a7a171
astrapi69 <asterios.raptis@gmx.net>
2014-12-06 13:13:10 +0100

    new function added.

--- jaulp.wicket.behaviors.examples/src/main/java/org/jaulp/wicket/behaviors/examples/functions.js
+++ jaulp.wicket.behaviors.examples/src/main/java/org/jaulp/wicket/behaviors/examples/functions.js
@@ -1,8 +1,16 @@
+$(window).load(function(){
+	$("a[class^='navbarlink']").click(function () {
+		$(this).siblings().removeClass('navbarlink-underline');
+		$(this).toggleClass('navbarlink-underline');
+	});
+});
+
 function setFocus(comp) {
 	comp.focus();
 	comp.select();
 }
 
 function alertnow(){
-	alert('foo bar');
+
 }
+
