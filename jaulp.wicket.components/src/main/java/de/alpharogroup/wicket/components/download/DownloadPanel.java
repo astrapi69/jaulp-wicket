909ba6f9dc83a5c357c6b900a31afa304527d65b
astrapi69 <asterios.raptis@gmx.net>
2014-12-19 17:22:49 +0100

    replaced depracated methods

--- jaulp.wicket.components/src/main/java/de/alpharogroup/wicket/components/download/DownloadPanel.java
+++ jaulp.wicket.components/src/main/java/de/alpharogroup/wicket/components/download/DownloadPanel.java
@@ -10,7 +10,7 @@ import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.util.lang.Args;
 import org.apache.wicket.util.resource.IResourceStream;
 import org.jaulp.wicket.base.BasePanel;
-import org.jaulp.wicket.base.util.WicketComponentUtils;
+import org.jaulp.wicket.base.util.ApplicationUtils;
 import org.jaulp.wicket.behaviors.AjaxDownloadBehavior;
 
 public abstract class DownloadPanel extends BasePanel<DownloadModel> {
@@ -26,7 +26,7 @@ public abstract class DownloadPanel extends BasePanel<DownloadModel> {
 			@Override
 			protected IResourceStream getResourceStream() {
 				try {
-					return WicketComponentUtils.getResourceStream(getWebApplication(), model.getObject().getPath(),model.getObject().getContentType());
+					return ApplicationUtils.getResourceStream(getWebApplication(), model.getObject().getPath(),model.getObject().getContentType());
 				} catch (IOException e) {
 					e.printStackTrace();
 					getSession().info("Error: "+e.getLocalizedMessage());
