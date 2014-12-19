fac513bfa1e485ecebbc45b65238d7d79d713098
astrapi69 <asterios.raptis@gmx.net>
2014-12-19 17:23:33 +0100

    Changed radiochoice panel with enum values.

--- jaulp.wicket.components.examples/src/main/java/de/alpharogroup/wicket/components/examples/radiochoices/RadioChoicePanel.java
+++ jaulp.wicket.components.examples/src/main/java/de/alpharogroup/wicket/components/examples/radiochoices/RadioChoicePanel.java
@@ -1,4 +1,5 @@
 package de.alpharogroup.wicket.components.examples.radiochoices;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -6,6 +7,7 @@ import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormChoiceComponentUpdatingBehavior;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.markup.html.form.RadioChoice;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
@@ -19,32 +21,57 @@ public class RadioChoicePanel extends Panel {
 	 * The serialVersionUID
 	 */
 	private static final long serialVersionUID = 1L;
-	private static final List<String> TYPES = Arrays
-			.asList(new String[] { "Lamborgini", "Maserati", "Ferrari", "Porsche" });
-	
-	private String selected = "";
+	private static final List<Brands> TYPES = Arrays.asList(Brands.values());
+
+	private Brands selected
+//	= Brands.LAMBORGINI
+	;
+
 	public RadioChoicePanel(String id, IModel<?> model) {
 		super(id, model);
-		RadioChoice<String> brandingType = new RadioChoice<String>(
-				"branding", new PropertyModel<String>(this, "selected"), TYPES);
+		IChoiceRenderer<Brands> renderer = new IChoiceRenderer<Brands>() {
+			private static final long serialVersionUID = 1L;
+
+			public Object getDisplayValue(Brands object) {
+				return object.getValue();
+			}
+
+			public String getIdValue(Brands object, int index) {
+				return object.getValue();
+			}
+			public Brands getObject(String id,
+					IModel<? extends List<? extends Brands>> choices) {
+				for (Brands brand : choices.getObject()) {
+					if(brand.getValue().equals(id)) {
+						return brand;
+					}
+				}
+				return null;
+			}
+
+		};
+		RadioChoice<Brands> brandingType = new RadioChoice<Brands>("branding",
+				new PropertyModel<Brands>(this, "selected"), TYPES, renderer);
+		// dont add a <br> after every radio...
+		brandingType.setSuffix("");
 		brandingType.setOutputMarkupId(true);
 		brandingType.add(new AjaxFormChoiceComponentUpdatingBehavior() {
 			private static final long serialVersionUID = 1L;
 
-	        @Override 
-	        protected void onUpdate(AjaxRequestTarget target) { 
-				target.add(getFeedback()); 
-				info("Selected Type : " + selected);
-	        } 
+			@Override
+			protected void onUpdate(AjaxRequestTarget target) {
+				target.add(getFeedback());
+				info("Selected Type : " + selected.getValue());
+			}
 		});
 		Form<?> form = new Form<Void>("form");
- 
+
 		add(form);
 		form.add(brandingType);
 	}
-	
+
 	protected Component getFeedback() {
 		PubliclyBasePage<?> basePage = (PubliclyBasePage<?>) getPage();
-		return basePage.getFeedback();		
+		return basePage.getFeedback();
 	}
 }
