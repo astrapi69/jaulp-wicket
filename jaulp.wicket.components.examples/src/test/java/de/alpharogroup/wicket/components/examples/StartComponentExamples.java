7446dac8481aee55240c62bf16239e30b470265c
astrapi69 <asterios.raptis@gmx.net>
2015-03-14 16:40:09 +0100

    created new methods to start jetty server.

--- jaulp.wicket.components.examples/src/test/java/de/alpharogroup/wicket/components/examples/StartComponentExamples.java
+++ jaulp.wicket.components.examples/src/test/java/de/alpharogroup/wicket/components/examples/StartComponentExamples.java
@@ -15,14 +15,6 @@
  */
 package de.alpharogroup.wicket.components.examples;
 
-import org.apache.wicket.util.time.Duration;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.bio.SocketConnector;
-import org.eclipse.jetty.server.ssl.SslSocketConnector;
-import org.eclipse.jetty.util.resource.Resource;
-import org.eclipse.jetty.util.ssl.SslContextFactory;
-import org.eclipse.jetty.webapp.WebAppContext;
-
 import java.io.File;
 import java.util.EnumSet;
 
@@ -31,15 +23,96 @@ import javax.servlet.DispatcherType;
 import org.apache.wicket.Application;
 import org.apache.wicket.protocol.http.ContextParamWebApplicationFactory;
 import org.apache.wicket.protocol.http.WicketFilter;
+import org.apache.wicket.util.time.Duration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.bio.SocketConnector;
+import org.eclipse.jetty.server.ssl.SslSocketConnector;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 import de.alpharogroup.wicket.components.examples.application.WicketApplication;
 
 public class StartComponentExamples
 {
 	public static void main(String[] args) throws Exception
+	{
+		 eclipseStart();
+		
+		// alternate start with params...
+//		File webapp = PathFinder.getSrcMainJavaDir();
+//		new File(PathFinder.getSrcMainJavaDir(), "webapp");
+//		startWebapp(WicketApplication.class, "/", webapp, WicketApplication.DEFAULT_HTTP_PORT,
+//			WicketApplication.DEFAULT_HTTPS_PORT, 300, "/*");
+	}
+
+
+	/**
+	 * Starts the given wicket application with the given parameters.
+	 * 
+	 * @param applicationClass
+	 * @param contextPath
+	 * @param webapp
+	 * @param httpPort
+	 * @param httpsPort
+	 * @param maxInactiveInterval
+	 * @param filterPath
+	 * @throws Exception
+	 */
+	public static void startWebapp(final Class<? extends Application> applicationClass,
+		String contextPath, File webapp, int httpPort, int maxInactiveInterval, String filterPath)
+		throws Exception
+	{
+		startWebapp(applicationClass, contextPath, webapp, httpPort, 8443, maxInactiveInterval,
+			filterPath);
+	}
+
+
+	/**
+	 * Starts the given wicket application with the given parameters.
+	 * 
+	 * @param applicationClass
+	 * @param contextPath
+	 * @param webapp
+	 * @param httpPort
+	 * @param httpsPort
+	 * @param maxInactiveInterval
+	 * @param filterPath
+	 * @throws Exception
+	 */
+	public static void startWebapp(final Class<? extends Application> applicationClass,
+		String contextPath, File webapp, int httpPort, int httpsPort, int maxInactiveInterval,
+		String filterPath) throws Exception
+	{
+		final Server server = new Server(httpPort);
+
+		final ServletContextHandler context = new ServletContextHandler(
+			ServletContextHandler.SESSIONS);
+		context.setContextPath(contextPath);
+
+		context.setResourceBase(webapp.getAbsolutePath());
+
+		final FilterHolder filter = new FilterHolder(WicketFilter.class);
+		filter.setInitParameter(ContextParamWebApplicationFactory.APP_CLASS_PARAM,
+			applicationClass.getName());
+		filter.setInitParameter(WicketFilter.FILTER_MAPPING_PARAM, filterPath);
+		context.addFilter(filter, filterPath,
+			EnumSet.of(DispatcherType.REQUEST, DispatcherType.ERROR));
+		context.addServlet(DefaultServlet.class, filterPath);
+
+		context.getSessionHandler().getSessionManager().setMaxInactiveInterval(maxInactiveInterval);
+
+		server.setHandler(context);
+
+		server.start();
+		server.join();
+
+	}
+
+	private static void eclipseStart()
 	{
 		int timeout = (int)Duration.ONE_HOUR.getMilliseconds();
 
@@ -110,47 +183,5 @@ public class StartComponentExamples
 			System.exit(1);
 		}
 	}
-	
-	
-  /**
-   * Starts the given wicket application with the given parameters.
-   * @param applicationClass
-   * @param contextPath
-   * @param webapp
-   * @param httpPort
-   * @param httpsPort
-   * @param maxInactiveInterval
-   * @param filterPath
-   * @throws Exception
-   */
-  public static void startWebapp(
-    final Class<? extends Application> applicationClass,
-    String contextPath,
-    File webapp,
-    int httpPort,
-    int httpsPort,
-    int maxInactiveInterval,
-    String filterPath) throws Exception {
 
-    final Server server = new Server(httpPort);
-
-    final ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-    context.setContextPath(contextPath);
-
-    context.setResourceBase(webapp.getAbsolutePath());
-
-    final FilterHolder filter = new FilterHolder(WicketFilter.class);
-    filter.setInitParameter(ContextParamWebApplicationFactory.APP_CLASS_PARAM, applicationClass.getName());
-    filter.setInitParameter(WicketFilter.FILTER_MAPPING_PARAM, filterPath);
-    context.addFilter(filter, filterPath, EnumSet.of(DispatcherType.REQUEST, DispatcherType.ERROR));
-    context.addServlet(DefaultServlet.class, filterPath);
-
-    context.getSessionHandler().getSessionManager().setMaxInactiveInterval(maxInactiveInterval);
-
-    server.setHandler(context);
-
-    server.start();
-    server.join();
-
-  }
 }
