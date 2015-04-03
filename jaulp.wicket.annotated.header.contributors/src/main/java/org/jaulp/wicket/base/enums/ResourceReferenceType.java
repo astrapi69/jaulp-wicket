77dbdae75263753d92e05189eac296d4eab2c430
astrapi69 <asterios.raptis@gmx.net>
2015-04-03 10:29:13 +0200

    New type for less.

--- jaulp.wicket.annotated.header.contributors/src/main/java/org/jaulp/wicket/base/enums/ResourceReferenceType.java
+++ jaulp.wicket.annotated.header.contributors/src/main/java/org/jaulp/wicket/base/enums/ResourceReferenceType.java
@@ -23,7 +23,9 @@ public enum ResourceReferenceType
 
 	/** The type for javascript ResourceReferences. */
 	JS,
-	/** The type for css ResourceReferences. */
-	CSS;
+  /** The type for less ResourceReferences. */
+  LESS,
+  /** The type for css ResourceReferences. */
+  CSS;
 
 }
