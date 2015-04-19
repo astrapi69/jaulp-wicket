40545d7b365663440167f0de730a8f0e0aa1dff6
astrapi69 <asterios.raptis@gmx.net>
2015-04-19 13:13:10 +0200

    New panel for signin examples, and added some bootstrap decoration to the signinpanel.

---
+++ jaulp.wicket.components.examples/src/main/java/de/alpharogroup/wicket/components/examples/sign/in/SigninExamplesPanel.java
@@ -0,0 +1,136 @@
+package de.alpharogroup.wicket.components.examples.sign.in;
+
+import net.sourceforge.jaulp.auth.models.SignInModel;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.panel.GenericPanel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
+import org.jaulp.wicket.behaviors.BuildableChainableStatement;
+import org.jaulp.wicket.behaviors.JqueryStatementsBehavior;
+import org.odlabs.wiquery.core.javascript.JsUtils;
+
+import de.alpharogroup.wicket.components.examples.area.publicly.PubliclyBasePage;
+import de.alpharogroup.wicket.components.labeled.textfield.LabeledEmailTextFieldPanel;
+import de.alpharogroup.wicket.components.labeled.textfield.LabeledPasswordTextFieldPanel;
+import de.alpharogroup.wicket.components.sign.in.SigninPanel;
+import de.alpharogroup.wicket.components.sign.in.form.SigninFormPanel;
+
+public class SigninExamplesPanel extends GenericPanel<SignInModel>
+{
+
+	/**
+	 * The serialVersionUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SigninExamplesPanel(final String id, final IModel<SignInModel> model)
+	{
+		super(id);
+
+		final IModel<SignInModel> cpm = new CompoundPropertyModel<SignInModel>(model);
+		SigninFormPanel<SignInModel> signFormPanel = new SigninFormPanel<SignInModel>(
+			"horizantalFormPanel", cpm)
+		{
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void onSignin(AjaxRequestTarget target, Form<?> form)
+			{
+				target.add(getFeedback());
+				info("Email: " + getModelObject().getEmail() + "\nPassword:"
+					+ getModelObject().getPassword());
+			}
+
+			@Override
+			protected Form<?> newForm(String id, IModel<?> model)
+			{
+				Form<?> form = super.newForm(id, model);
+				form.add(new AttributeAppender("class", " form-horizontal"));
+				return form;
+			}
+
+			@Override
+			protected Button newButton(String id)
+			{
+				Button button = super.newButton(id);
+				button.add(
+					new JqueryStatementsBehavior().add(new BuildableChainableStatement.Builder()
+						.label("wrap").args(JsUtils.quotes("<div class=\"form-group\"></div>"))
+						.build())).add(
+					new JqueryStatementsBehavior().add(new BuildableChainableStatement.Builder()
+						.label("wrap")
+						.args(JsUtils.quotes("<div class=\"col-sm-offset-2 col-sm-10\"></div>"))
+						.build()));
+				button.add(new AttributeAppender("class", " btn btn-default"));
+				return button;
+			}
+
+			@Override
+			protected Component newSigninPanel(String id, IModel<? extends SignInModel> model)
+			{
+				SigninPanel signinPanel = new SigninPanel(id, model)
+				{
+					/**
+					 * The serialVersionUID
+					 */
+					private static final long serialVersionUID = 1L;
+
+					@SuppressWarnings("unchecked")
+					@Override
+					protected Component newEmailTextField(String id, IModel<SignInModel> model)
+					{
+						final LabeledEmailTextFieldPanel<SignInModel> emailTextField = (LabeledEmailTextFieldPanel<SignInModel>)super
+							.newEmailTextField(id, model);
+						emailTextField.add(new AttributeAppender("class", " form-group"));
+						emailTextField
+							.getEmailTextField()
+							.add(
+								new JqueryStatementsBehavior()
+									.add(new BuildableChainableStatement.Builder().label("wrap")
+										.args(JsUtils.quotes("<div class=\"col-sm-10\"></div>"))
+										.build()))
+							.add(new AttributeAppender("class", " form-control"));
+						emailTextField.getLabelComponent().add(
+							new AttributeAppender("class", " control-label col-sm-2"));
+						return emailTextField;
+					}
+
+					@SuppressWarnings("unchecked")
+					@Override
+					protected Component newPasswordTextField(String id, IModel<SignInModel> model)
+					{
+						final LabeledPasswordTextFieldPanel<SignInModel> pwTextField = (LabeledPasswordTextFieldPanel<SignInModel>)super
+							.newPasswordTextField(id, model);
+						pwTextField.add(new AttributeAppender("class", " form-group"));
+						pwTextField
+							.getPasswordTextField()
+							.add(
+								new JqueryStatementsBehavior()
+									.add(new BuildableChainableStatement.Builder().label("wrap")
+										.args(JsUtils.quotes("<div class=\"col-sm-10\"></div>"))
+										.build()))
+							.add(new AttributeAppender("class", " form-control"));
+						pwTextField.getLabelComponent().add(
+							new AttributeAppender("class", " control-label col-sm-2"));
+						return pwTextField;
+					}
+				};
+				return signinPanel;
+			}
+
+		};
+		add(signFormPanel);
+	}
+
+	protected Component getFeedback()
+	{
+		PubliclyBasePage<?> basePage = (PubliclyBasePage<?>)getPage();
+		return basePage.getFeedback();
+	}
+
+}
--- jaulp.wicket.components.examples/src/main/java/de/alpharogroup/wicket/components/examples/sign/in/SigninPage.java
+++ jaulp.wicket.components.examples/src/main/java/de/alpharogroup/wicket/components/examples/sign/in/SigninPage.java
@@ -20,35 +20,19 @@ import net.sourceforge.jaulp.auth.models.SignInModel;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.IModel;
 import org.wicketstuff.annotation.mount.MountPath;
 
 import de.alpharogroup.wicket.components.examples.area.publicly.PubliclyBasePage;
-import de.alpharogroup.wicket.components.sign.in.form.SigninFormPanel;
 
 @MountPath("public/signin")
-public class SigninPage extends PubliclyBasePage<Object>
+public class SigninPage extends PubliclyBasePage<SignInModel>
 {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	public Component getContainerPanel()
 	{
-		final SignInModel model = new BaseSignInModel();
-
-		final IModel<SignInModel> cpm = new CompoundPropertyModel<SignInModel>(model);
-		SigninFormPanel signFormPanel = new SigninFormPanel(CONTAINER_PANEL_ID, cpm)
-		{
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			protected void onSignin()
-			{
-				System.out.println(model.getEmail());
-				System.out.println(model.getPassword());
-
-			}
-		};
-		return signFormPanel;
+		return new SigninExamplesPanel(CONTAINER_PANEL_ID, new CompoundPropertyModel<SignInModel>(
+			new BaseSignInModel()));
 	}
 }
