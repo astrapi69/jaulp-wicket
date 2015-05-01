096f1a889402f5b945418725e6867ec7db95498d
astrapi69 <asterios.raptis@gmx.net>
2015-05-01 12:06:40 +0200

    created class to get data for the dropdowns.

---
+++ jaulp.wicket.dropdownchoices.examples/src/main/java/org/jaulp/wicket/dropdownchoices/pages/DatabaseManager.java
@@ -0,0 +1,30 @@
+package org.jaulp.wicket.dropdownchoices.pages;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DatabaseManager implements Serializable
+{
+
+
+	public static Map<String, List<String>> initializeModelMap()
+	{
+		/** The models map. */
+		Map<String, List<String>> modelsMap = new HashMap<String, List<String>>();
+
+		modelsMap.put("trademark.audi",
+			Arrays.asList(new String[] { "audi.a4", "audi.a6", "audi.tt" }));
+		modelsMap.put(
+			"trademark.cadillac",
+			Arrays.asList(new String[] { "cadillac.cts", "cadillac.dts", "cadillac.escalade",
+					"cadillac.srx", "cadillac.deville" }));
+		modelsMap.put(
+			"trademark.ford",
+			Arrays.asList(new String[] { "ford.crown", "ford.escape", "ford.expedition",
+					"ford.explorer", "ford.f_150" }));
+		return modelsMap;
+	}
+}
