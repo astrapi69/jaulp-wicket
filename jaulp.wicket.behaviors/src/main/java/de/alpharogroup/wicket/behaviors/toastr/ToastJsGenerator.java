62f031277d48b8ee87cc285d325bf5ebbb321f4e
astrapi69 <asterios.raptis@gmx.net>
2015-05-29 14:57:29 +0200

    extends now from JavascriptGenerator.

--- jaulp.wicket.behaviors/src/main/java/de/alpharogroup/wicket/behaviors/toastr/ToastJsGenerator.java
+++ jaulp.wicket.behaviors/src/main/java/de/alpharogroup/wicket/behaviors/toastr/ToastJsGenerator.java
@@ -16,139 +16,103 @@
 package de.alpharogroup.wicket.behaviors.toastr;
 
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.Map;
-
-import lombok.Getter;
+import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.wicket.request.resource.ResourceReference;
 import org.apache.wicket.util.lang.Args;
-import org.apache.wicket.util.template.PackageTextTemplate;
-import org.apache.wicket.util.template.TextTemplate;
 
-import de.alpharogroup.wicket.base.util.template.TextTemplateUtils;
+import de.alpharogroup.wicket.base.util.template.JavascriptGenerator;
+import de.alpharogroup.wicket.base.util.template.StringTextValue;
 
 /**
  * The Class ToastJsGenerator generates the javascript with a ToastrSettings object.
  */
-public class ToastJsGenerator implements Serializable
-{
+public class ToastJsGenerator extends JavascriptGenerator<ToastrSettings> implements Serializable {
 
-	/**
-	 * The serialVersionUID
-	 */
-	private static final long serialVersionUID = 1L;
+  /**
+   * The serialVersionUID
+   */
+  private static final long serialVersionUID = 1L;
 
-	/** The {@link ResourceReference} constant for the js and css-file from the toastr plugin. */
-	public static final ResourceReference TOASTR_PLUGIN_REFERENCE = new ToastrResourceReference();
+  /**
+   * The constant for the key of the command.
+   */
+  public static final String COMMAND = "command";
 
-	/** The toastr template. */
-	public static final TextTemplate TOASTR_TEMPLATE = new PackageTextTemplate(
-		ToastJsGenerator.class, "toastr-template.js.tmpl");
+  /**
+   * Instantiates a new {@link ToastJsGenerator}.
+   */
+  public ToastJsGenerator() {
+    this(new ToastrSettings());
+  }
 
-	/**
-	 * The settings.
-	 */
-	@Getter
-	private ToastrSettings settings;
+  /**
+   * Instantiates a new {@link ToastJsGenerator} with the given {@link ToastrSettings}.
+   * @param settings
+   *            the settings for the toastr plugin.
+   */
+  public ToastJsGenerator(ToastrSettings settings) {
+    super(Args.notNull(settings, "settings"));
+  }
 
-	/**
-	 * Instantiates a new toast js generator.
-	 */
-	public ToastJsGenerator()
-	{
-	}
+  /**
+   * {@inheritDoc}.
+   */
+  @Override
+  protected Map<String, Object> initializeVariables(final Set<StringTextValue<?>> allSettings)
+  {
+    final Map<String, Object> variables = super.initializeVariables(allSettings);
+    variables.put(COMMAND, getCommand(getSettings()));
+    return variables;
+  }
 
-	/**
-	 * Instantiates a new toast js generator.
-	 *
-	 * @param settings
-	 *            the settings
-	 */
-	public ToastJsGenerator(ToastrSettings settings)
-	{
-		this.settings = Args.notNull(settings, "settings");
-	}
+  /**
+   * Generates the javascript template code from the given map and the given method name that will
+   * be used to interpolate with the values of the given map.
+   * @param variables the map with the javascript options.
+   * @param methodName The method name.
+   * @return The generated javascript from the given map and the given method name.
+   */
+  public String generateJavascriptTemplateContent(final Map<String, Object> variables,
+                                                  String methodName)
+  {
+    StringBuilder sb = new StringBuilder();
+    sb.append(DOCUMENT_READY_FUNCTION_PREFIX).append("\n").append("\n");
+    for (Map.Entry<String, Object> entry : variables.entrySet()) {
+      String key = entry.getKey();
+      if (!COMMAND.equals(key)) {
+        sb.append(key).append("=${").append(key).append("};");
+        sb.append("\n");
+      }
+    }
+    sb.append("\n");
+    sb.append("${" + COMMAND + "};");
+    sb.append("\n");
+    sb.append(DOCUMENT_READY_FUNCTION_SUFFIX);
+    return sb.toString();
+  }
 
-	/**
-	 * Generate toastr js.
-	 *
-	 * @param settings
-	 *            the settings
-	 * @return the string
-	 */
-	public String generateToastrJs(ToastrSettings settings)
-	{
-		return generateToastrJs(TOASTR_TEMPLATE, settings);
-		// return null;
-	}
+  /**
+   * Gets the command.
+   * @param settings the toastrSettings
+   * @return the command
+   */
+  public String getCommand(ToastrSettings settings)
+  {
+    StringBuilder sb = new StringBuilder();
+    sb.append("toastr.");
+    sb.append(settings.getToastrType().getValue().getValue());
+    sb.append("('");
+    sb.append(settings.getNotificationContent().getValue());
+    sb.append("'");
+    if (StringUtils.isNotEmpty(settings.getNotificationTitle().getValue())) {
+      sb.append(", '");
+      sb.append(settings.getNotificationTitle().getValue());
+      sb.append("'");
+    }
+    sb.append(")");
+    return sb.toString();
+  }
 
-	/**
-	 * Generate toastr js.
-	 *
-	 * @param textTemplate
-	 *            the text template
-	 * @param settings
-	 *            the settings
-	 * @return the string
-	 */
-	public String generateToastrJs(final TextTemplate textTemplate, ToastrSettings settings)
-	{
-		final Map<String, Object> variables = new HashMap<>();
-		variables.put("command", getCommand(settings));
-		variables.put("closeButton", settings.getCloseButton().getValue());
-		variables.put("debug", settings.getDebug().getValue());
-		variables.put("newestOnTop", settings.getNewestOnTop().getValue());
-		variables.put("progressBar", settings.getProgressBar().getValue());
-		TextTemplateUtils.setVariableWithSingleQuotationMarks("positionClass", settings
-			.getPositionClass().getValue(), variables);
-		variables.put("preventDuplicates", settings.getPreventDuplicates().getValue());
-		TextTemplateUtils.setVariableWithSingleQuotationMarks("onclick", settings.getOnclick()
-			.getValue(), variables);
-		TextTemplateUtils.setVariableWithSingleQuotationMarks("showDuration", settings
-			.getShowDuration().getValue(), variables);
-		TextTemplateUtils.setVariableWithSingleQuotationMarks("hideDuration", settings
-			.getHideDuration().getValue(), variables);
-		TextTemplateUtils.setVariableWithSingleQuotationMarks("timeOut", settings.getTimeOut()
-			.getValue(), variables);
-		TextTemplateUtils.setVariableWithSingleQuotationMarks("extendedTimeOut", settings
-			.getExtendedTimeOut().getValue(), variables);
-		TextTemplateUtils.setVariableWithSingleQuotationMarks("showEasing", settings
-			.getShowEasing().getValue(), variables);
-		TextTemplateUtils.setVariableWithSingleQuotationMarks("hideEasing", settings
-			.getHideEasing().getValue(), variables);
-		TextTemplateUtils.setVariableWithSingleQuotationMarks("showMethod", settings
-			.getShowMethod().getValue(), variables);
-		TextTemplateUtils.setVariableWithSingleQuotationMarks("hideMethod", settings
-			.getHideMethod().getValue(), variables);
-		variables.put("tapToDismiss", settings.getTapToDismiss().getValue());
-		textTemplate.interpolate(variables);
-		return textTemplate.asString();
-	}
-
-	/**
-	 * Gets the command.
-	 *
-	 * @param settings
-	 *            the settings
-	 * @return the command
-	 */
-	public String getCommand(ToastrSettings settings)
-	{
-		StringBuilder sb = new StringBuilder();
-		sb.append("toastr.");
-		sb.append(settings.getToastrType().getValue());
-		sb.append("('");
-		sb.append(settings.getNotificationContent().getValue());
-		sb.append("'");
-		if (StringUtils.isNotEmpty(settings.getNotificationTitle().getValue()))
-		{
-			sb.append(", '");
-			sb.append(settings.getNotificationTitle().getValue());
-			sb.append("'");
-		}
-		sb.append(")");
-		return sb.toString();
-	}
 }
