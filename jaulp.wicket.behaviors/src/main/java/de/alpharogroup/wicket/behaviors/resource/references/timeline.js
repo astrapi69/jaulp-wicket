63b121f264d0922020e29022141dad9b2ffc9c9e
astrapi69 <asterios.raptis@gmx.net>
2015-05-29 14:56:49 +0200

    source code formatted.

--- jaulp.wicket.behaviors/src/main/java/de/alpharogroup/wicket/behaviors/resource/references/timeline.js
+++ jaulp.wicket.behaviors/src/main/java/de/alpharogroup/wicket/behaviors/resource/references/timeline.js
@@ -12,7 +12,8 @@ function drawChart() {
   dataTable.addRows([
     [ 'Washington', new Date(1789, 3, 29), new Date(1797, 2, 3) ],
     [ 'Adams',      new Date(1797, 2, 3),  new Date(1801, 2, 3) ],
-    [ 'Jefferson',  new Date(1801, 2, 3),  new Date(1809, 2, 3) ]]);
+    [ 'Jefferson',  new Date(1801, 2, 3),  new Date(1809, 2, 3) ]
+  ]);
 
   chart.draw(dataTable);
 }
