97f825d74cc293d802f70cee5ce384813ea17b6f
astrapi69 <asterios.raptis@gmx.net>
2015-06-04 09:16:24 +0200

    new class PercentNumberFormatTextValue created for set percent values.

---
+++ jaulp.wicket.base/src/test/java/de/alpharogroup/wicket/base/util/template/PercentNumberFormatTextValueTest.java
@@ -0,0 +1,57 @@
+package de.alpharogroup.wicket.base.util.template;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+/**
+ * Unit test class for class PercentNumberFormatTextValue.
+ */
+public class PercentNumberFormatTextValueTest {
+
+
+  @Test
+  public void test()
+  {
+
+    PercentNumberFormatTextValue textValue = new PercentNumberFormatTextValue("foo", 2);
+    String actual = textValue.getValue();
+    String expected = "2%";
+    Assert.assertEquals(expected, actual);
+    // Give a int value...
+    textValue.setValue(3);
+    actual = textValue.getValue();
+    expected = "3%";
+    Assert.assertEquals(expected, actual);
+    System.out.println(actual);
+    // Give a string value...
+    textValue.setValue("5");
+    actual = textValue.getValue();
+    expected = "5%";
+    Assert.assertEquals(expected, actual);
+    // Give a string value...
+    textValue.setValue("5%");
+    actual = textValue.getValue();
+    expected = "5%";
+    Assert.assertEquals(expected, actual);
+    // Give a string value that is not conform...
+    textValue.setValue("xy");
+    actual = textValue.getValue();
+    expected = "50%";
+    Assert.assertEquals(expected, actual);
+    // Give a int value that is not conform...
+    textValue.setValue(-1);
+    actual = textValue.getValue();
+    expected = "50%";
+    Assert.assertEquals(expected, actual);
+    // Give a int value that is not conform...
+    textValue.setValue(100);
+    actual = textValue.getValue();
+    expected = "100%";
+    Assert.assertEquals(expected, actual);
+    // Give a int value that is not conform...
+    textValue.setValue(0);
+    actual = textValue.getValue();
+    expected = "0%";
+    Assert.assertEquals(expected, actual);
+  }
+}
