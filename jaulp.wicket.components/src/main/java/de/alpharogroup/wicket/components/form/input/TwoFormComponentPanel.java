58f2e364ec6dd0385b71cf8dacf1e44b1cab42ea
astrapi69 <asterios.raptis@gmx.net>
2015-06-12 09:46:25 +0200

    organize imports.

--- jaulp.wicket.components/src/main/java/de/alpharogroup/wicket/components/form/input/TwoFormComponentPanel.java
+++ jaulp.wicket.components/src/main/java/de/alpharogroup/wicket/components/form/input/TwoFormComponentPanel.java
@@ -19,7 +19,6 @@ import java.io.Serializable;
 
 import lombok.Getter;
 
-import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.html.form.FormComponentPanel;
