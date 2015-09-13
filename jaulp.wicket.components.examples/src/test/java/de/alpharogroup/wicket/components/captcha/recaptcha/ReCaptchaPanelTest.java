58e593147d38e267032b7f52be46c6029df85019
astrapi69 <asterios.raptis@gmx.net>
2015-09-13 18:05:10 +0200

    new patch version created.

--- jaulp.wicket.components.examples/src/test/java/de/alpharogroup/wicket/components/captcha/recaptcha/ReCaptchaPanelTest.java
+++ jaulp.wicket.components.examples/src/test/java/de/alpharogroup/wicket/components/captcha/recaptcha/ReCaptchaPanelTest.java
@@ -15,42 +15,47 @@
  */
 package de.alpharogroup.wicket.components.captcha.recaptcha;
 
+import org.apache.wicket.util.tester.WicketTester;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import de.alpharogroup.wicket.components.examples.application.WicketApplication;
 
 
 public class ReCaptchaPanelTest
 {
 
-	// private WicketTester tester;
+	private WicketTester tester;
 
-	// @BeforeMethod
-	// public void setUp() throws Exception
-	// {
-	// tester = new WicketTester(new WicketApplication());
-	// }
-	//
-	// @Test
-	// public void testRender() throws Exception
-	// {
-	// tester.startComponentInPage(new ReCaptchaPanel("panel")
-	// {
-	// private static final long serialVersionUID = 1L;
-	// private final static String PUBLIC_KEY = "6LdVBfwSAAAAAMz07R7X5rmdn3sNiHG2UfM_5IUl";
-	// private final static String PRIVATE_KEY = "6LdVBfwSAAAAAKsMpZcDlpl_F1pji5JcnmNt-Hon";
-	//
-	// @Override
-	// public String getPrivateKey()
-	// {
-	// return PRIVATE_KEY;
-	// }
-	//
-	// @Override
-	// public String getPublicKey()
-	// {
-	// return PUBLIC_KEY;
-	// }
-	//
-	// });
-	// tester.assertComponent("panel", ReCaptchaPanel.class);
-	// tester.assertVisible("panel");
-	// }
+	@BeforeMethod
+	public void setUp() throws Exception
+	{
+		tester = new WicketTester(new WicketApplication());
+	}
+
+	@Test
+	public void testRender() throws Exception
+	{
+		tester.startComponentInPage(new ReCaptchaPanel("panel")
+		{
+			private static final long serialVersionUID = 1L;
+			private final static String PUBLIC_KEY = "6LdVBfwSAAAAAMz07R7X5rmdn3sNiHG2UfM_5IUl";
+			private final static String PRIVATE_KEY = "6LdVBfwSAAAAAKsMpZcDlpl_F1pji5JcnmNt-Hon";
+
+			@Override
+			public String getPrivateKey()
+			{
+				return PRIVATE_KEY;
+			}
+
+			@Override
+			public String getPublicKey()
+			{
+				return PUBLIC_KEY;
+			}
+
+		});
+		tester.assertComponent("panel", ReCaptchaPanel.class);
+		tester.assertVisible("panel");
+	}
 }
--- jaulp.wicket.components.examples/src/test/java/de/alpharogroup/wicket/components/examples/TestHomePage.java
+++ jaulp.wicket.components.examples/src/test/java/de/alpharogroup/wicket/components/examples/TestHomePage.java
@@ -15,6 +15,12 @@
  */
 package de.alpharogroup.wicket.components.examples;
 
+import org.apache.wicket.util.tester.WicketTester;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import de.alpharogroup.wicket.components.examples.application.WicketApplication;
+import de.alpharogroup.wicket.components.examples.home.HomePage;
 
 
 /**
@@ -22,21 +28,21 @@ package de.alpharogroup.wicket.components.examples;
  */
 public class TestHomePage
 {
-	// private WicketTester tester;
-	//
-	// @Test
-	// public void homepageRendersSuccessfully()
-	// {
-	// // start and render the test page
-	// tester.startPage(HomePage.class);
-	//
-	// // assert rendered page class
-	// tester.assertRenderedPage(HomePage.class);
-	// }
+	private WicketTester tester;
 
-	// @BeforeMethod
-	// public void setUp()
-	// {
-	// tester = new WicketTester(new WicketApplication());
-	// }
+	@Test
+	public void homepageRendersSuccessfully()
+	{
+		// start and render the test page
+		tester.startPage(HomePage.class);
+
+		// assert rendered page class
+		tester.assertRenderedPage(HomePage.class);
+	}
+
+	@BeforeMethod
+	public void setUp()
+	{
+		tester = new WicketTester(new WicketApplication());
+	}
 }
