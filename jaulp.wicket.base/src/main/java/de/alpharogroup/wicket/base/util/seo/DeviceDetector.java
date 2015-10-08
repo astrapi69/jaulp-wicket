814d192698589c4b4872097f1c1c9f125a73e324
astrapi69 <asterios.raptis@gmx.net>
2015-10-08 18:54:45 +0200

    initial version for device detection.

---
+++ jaulp.wicket.base/src/main/java/de/alpharogroup/wicket/base/util/seo/DeviceDetector.java
@@ -0,0 +1,12 @@
+package de.alpharogroup.wicket.base.util.seo;
+
+import java.io.Serializable;
+
+/**
+ * The Class DeviceDetector.
+ */
+public class DeviceDetector implements Serializable {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L; 	
+}
