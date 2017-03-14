fc166ab7ac6564426828e151162b8c96bc52ea79
Asterios Raptis <asterios.raptis@gmx.net>
2017-03-14 19:21:59 +0100

    merge of added new factory methods for ease of use.

--- jaulp.wicket.behaviors/src/main/java/de/alpharogroup/wicket/behaviors/FocusRequestBehavior.java
+++ jaulp.wicket.behaviors/src/main/java/de/alpharogroup/wicket/behaviors/FocusRequestBehavior.java
@@ -21,7 +21,7 @@ import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.OnLoadHeaderItem;
 
 /**
- * The Class FocusRequestBehavior set the focus on a component when the page is load.
+ * The class {@link FocusRequestBehavior} set the focus on a component when the page is load.
  */
 public class FocusRequestBehavior extends Behavior
 {
@@ -29,12 +29,70 @@ public class FocusRequestBehavior extends Behavior
 	/**
 	 * The serialVersionUID.
 	 */
-	private static final long serialVersionUID = 2312277970691939826L;
-	/** The Constant DEFAULT_ID is the default id that will be set if the id is not set explicit. */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The Constant DEFAULT_ID is the default id that will be set if the id is not set explicit.
+	 */
 	public static final String DEFAULT_ID = FocusRequestBehavior.class.getSimpleName();
 
+	/**
+	 * Factory method to create a new {@link FocusRequestBehavior} object for the component that is
+	 * added.
+	 *
+	 * @return the new {@link FocusRequestBehavior} object
+	 */
+	public static FocusRequestBehavior of()
+	{
+		return new FocusRequestBehavior();
+	}
+
+	/**
+	 * Factory method to create a new {@link FocusRequestBehavior} object from the given clearValue
+	 * parameter for the component that is added.
+	 *
+	 * @param clearValue
+	 *            the clear value
+	 * @return the new {@link FocusRequestBehavior} object
+	 */
+	public static FocusRequestBehavior of(final boolean clearValue)
+	{
+		return new FocusRequestBehavior(clearValue);
+	}
+
+	/**
+	 * Factory method to create a new {@link FocusRequestBehavior} object from the given clearValue
+	 * and delay parameter for the component that is added.
+	 *
+	 * @param clearValue
+	 *            the clear value
+	 * @param delay
+	 *            the delay
+	 * @return the new {@link FocusRequestBehavior} object
+	 */
+	public static FocusRequestBehavior of(final boolean clearValue, final Integer delay)
+	{
+		return new FocusRequestBehavior(clearValue, delay);
+	}
+
+	/**
+	 * Factory method to create a new {@link FocusRequestBehavior} object from the given delay
+	 * parameter for the component that is added.
+	 *
+	 * @param delay
+	 *            the delay
+	 * @return the new {@link FocusRequestBehavior} object
+	 */
+	public static FocusRequestBehavior of(final Integer delay)
+	{
+		return new FocusRequestBehavior(delay);
+	}
+
 	/** The flag if the value may be clear. */
-	private boolean clearValue;
+	private final boolean clearValue;
+
+	/** The delay that is set in the setTimeout method. */
+	private final int delay;
 
 	/**
 	 * Instantiates a new request focus behavior.
@@ -45,24 +103,49 @@ public class FocusRequestBehavior extends Behavior
 	}
 
 	/**
-	 * Instantiates a new focus request behavior.
+	 * Instantiates a new {@link FocusRequestBehavior} object.
 	 *
 	 * @param clearValue
 	 *            The flag if the value may be clear.
 	 */
 	public FocusRequestBehavior(final boolean clearValue)
 	{
-		this.clearValue = clearValue;
+		this(clearValue, 1);
 	}
 
 	/**
-	 * Creates the java script code for request focus.
+	 * Instantiates a new {@link FocusRequestBehavior} object.
+	 *
+	 * @param clearValue
+	 *            The flag if the value may be clear.
+	 * @param delay
+	 *            the delay
+	 */
+	public FocusRequestBehavior(final boolean clearValue, final Integer delay)
+	{
+		this.clearValue = clearValue;
+		this.delay = delay;
+	}
+
+	/**
+	 * Instantiates a new {@link FocusRequestBehavior} object.
+	 *
+	 * @param delay
+	 *            the delay
+	 */
+	public FocusRequestBehavior(final Integer delay)
+	{
+		this(false, delay);
+	}
+
+	/**
+	 * Factory method that creates the java script code for request focus.
 	 *
 	 * @param component
 	 *            the component
 	 * @return the string
 	 */
-	private String createJavaScript(final Component component)
+	protected String newJavaScript(final Component component)
 	{
 		final StringBuilder sb = new StringBuilder();
 		sb.append("setTimeout(" + "function() {" + "var component = document.getElementById(\"")
@@ -73,7 +156,7 @@ public class FocusRequestBehavior extends Behavior
 		}
 		sb.append("component.focus();");
 		sb.append("component.select();");
-		sb.append("}, 1)");
+		sb.append("}, " + this.delay + ")");
 		return sb.toString();
 	}
 
@@ -85,7 +168,8 @@ public class FocusRequestBehavior extends Behavior
 	{
 		super.renderHead(component, response);
 		component.setOutputMarkupId(true);
-		response.render(OnLoadHeaderItem.forScript(createJavaScript(component)));
+		response.render(OnLoadHeaderItem.forScript(newJavaScript(component)));
 		super.renderHead(component, response);
 	}
+
 }
--- jaulp.wicket.behaviors/src/main/java/de/alpharogroup/wicket/behaviors/JavascriptAppenderBehavior.java
+++ jaulp.wicket.behaviors/src/main/java/de/alpharogroup/wicket/behaviors/JavascriptAppenderBehavior.java
@@ -17,24 +17,112 @@ package de.alpharogroup.wicket.behaviors;
 
 import java.util.UUID;
 
-import lombok.Builder;
-
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
+import org.apache.wicket.markup.head.OnEventHeaderItem;
+import org.apache.wicket.markup.head.OnLoadHeaderItem;
 import org.apache.wicket.util.lang.Args;
+
+import lombok.Setter;
 
 /**
  * The Class JavascriptAppenderBehavior simply adds the given javascript code as String with an id
  * in the html page as script block.
  */
-@Builder
 public class JavascriptAppenderBehavior extends Behavior
 {
 
+	/**
+	 * The enum {@link JavascriptBindEvent} describes when the js will be bind.
+	 */
+	public enum JavascriptBindEvent
+	{
+		ONDOMREADY, ONEVENT, ONLOAD
+	}
+
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Factory method to create a new {@link JavascriptAppenderBehavior} object from the given
+	 * javascript.
+	 *
+	 * @param javascript
+	 *            the javascript
+	 * @return the new {@link JavascriptAppenderBehavior} object
+	 */
+	public static JavascriptAppenderBehavior of(final CharSequence javascript)
+	{
+		return new JavascriptAppenderBehavior(javascript);
+	}
+
+	/**
+	 * Factory method to create a new {@link JavascriptAppenderBehavior} object from the given
+	 * parameters.
+	 *
+	 * @param javascript
+	 *            the javascript
+	 * @param bindEvent
+	 *            the bind event
+	 * @return the new {@link JavascriptAppenderBehavior} object
+	 */
+	public static JavascriptAppenderBehavior of(final CharSequence javascript,
+		final JavascriptBindEvent bindEvent)
+	{
+		return new JavascriptAppenderBehavior(javascript, bindEvent);
+	}
+
+	/**
+	 * Factory method to create a new {@link JavascriptAppenderBehavior} object from the given
+	 * parameters.
+	 *
+	 * @param javascript
+	 *            javascript content to be add.
+	 * @param id
+	 *            unique id for the javascript element.
+	 * @return the new {@link JavascriptAppenderBehavior} object
+	 */
+	public static JavascriptAppenderBehavior of(final String id, final CharSequence javascript)
+	{
+		return new JavascriptAppenderBehavior(id, javascript);
+	}
+
+	/**
+	 * Factory method to create a new {@link JavascriptAppenderBehavior} object from the given
+	 * parameters.
+	 *
+	 * @param id
+	 *            the id
+	 * @param javascript
+	 *            the javascript
+	 * @param bindEvent
+	 *            the bind event
+	 * @return the new {@link JavascriptAppenderBehavior} object
+	 */
+	public static JavascriptAppenderBehavior of(final String id, final CharSequence javascript,
+		final JavascriptBindEvent bindEvent)
+	{
+		return new JavascriptAppenderBehavior(id, javascript, bindEvent);
+	};
+
+	/** The default bind event. */
+	private final JavascriptBindEvent DEFAULT_BIND_EVENT = JavascriptBindEvent.ONDOMREADY;
+
+
+	/** The target of the event handler. default window. */
+	@Setter
+	private String target = "window";
+
+	/** The event. default click. */
+	@Setter
+	private String event = "click";
+
+	/** The bind event for the javascript. */
+	@Setter
+	private JavascriptBindEvent bindEvent = DEFAULT_BIND_EVENT;
 
 	/**
 	 * The unique id for the javascript element. This can be null, however in that case the ajax
@@ -61,12 +149,46 @@ public class JavascriptAppenderBehavior extends Behavior
 	 *
 	 * @param javascript
 	 *            javascript content to be add.
+	 * @param bindEvent
+	 *            the bind event
+	 */
+	public JavascriptAppenderBehavior(final CharSequence javascript,
+		final JavascriptBindEvent bindEvent)
+	{
+		this(null, javascript, bindEvent);
+	}
+
+	/**
+	 * Instantiates a new {@link JavascriptAppenderBehavior}.
+	 *
+	 * @param javascript
+	 *            javascript content to be add.
 	 * @param id
 	 *            unique id for the javascript element.
 	 */
 	public JavascriptAppenderBehavior(final String id, final CharSequence javascript)
 	{
+		this(id, javascript, null);
+	}
+
+	/**
+	 * Instantiates a new {@link JavascriptAppenderBehavior}.
+	 *
+	 * @param javascript
+	 *            javascript content to be add.
+	 * @param id
+	 *            unique id for the javascript element.
+	 * @param bindEvent
+	 *            the bind event
+	 */
+	public JavascriptAppenderBehavior(final String id, final CharSequence javascript,
+		final JavascriptBindEvent bindEvent)
+	{
 		this.javascript = Args.notNull(javascript, "javascript");
+		if (bindEvent != null)
+		{
+			this.bindEvent = bindEvent;
+		}
 		if (id == null)
 		{
 			this.id = String.valueOf(UUID.randomUUID());
@@ -84,7 +206,21 @@ public class JavascriptAppenderBehavior extends Behavior
 	public void renderHead(final Component component, final IHeaderResponse response)
 	{
 		super.renderHead(component, response);
-		response.render(JavaScriptHeaderItem.forScript(this.javascript, this.id));
+		switch (bindEvent)
+		{
+			case ONDOMREADY :
+				response.render(OnDomReadyHeaderItem.forScript(this.javascript));
+				break;
+			case ONEVENT :
+				response.render(OnEventHeaderItem.forScript(target, event, this.javascript));
+				break;
+			case ONLOAD :
+				response.render(OnLoadHeaderItem.forScript(this.javascript));
+				break;
+			default :
+				response.render(JavaScriptHeaderItem.forScript(this.javascript, this.id));
+				break;
+		}
 	}
 
 }
