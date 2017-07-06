57959572a6eadc5af580fe9b8722cc636f7b7ea1
Asterios Raptis <asterios.raptis@gmx.net>
2017-07-06 11:58:48 +0200

    Create PayloadBean.java

---
+++ jaulp-wicket-behaviors/src/main/java/de/alpharogroup/wicket/behaviors/events/PayloadBean.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2010 Asterios Raptis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.alpharogroup.wicket.behaviors.events;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * The class {@link PayloadBean} is an event payload that can be sent through the wicket-system.
+ *
+ * @param <T> the generic type of the data to send with the payload
+ */
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class PayloadBean<T> {
+	
+	/** The payload. */
+	private T payload;
+	
+	/** The ajax request target. */
+	@NonNull
+	private final AjaxRequestTarget target;
+}
